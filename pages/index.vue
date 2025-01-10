432e3119985d24f737c4838e0eee5a25abf5a5b5
KITTIPONG WONGSADITYANON <kittipong.wong00@gmail.com>
2025-01-10 16:59:11 +0700

    Test

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-blue-500">
+      Hello Nuxt.js with Tailwind CSS!
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "IndexPage",
+});
+</script>
