2ff97ee4a2ba33467b17f037091290420b7d9163
KITTIPONG WONGSADITYANON <kittipong.wong00@gmail.com>
2025-01-14 13:36:25 +0700

    แก้ไข

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
-    
     <CardBox />
-    
   </div>
 </template>
 
 <script lang="ts">
-import Taps from "@/components/Taps/index.vue";
-import SearchFilter from "@/components/SearchFilter/index.vue"
 import CardBox from '@/components/Cards/index.vue';
 
 export default {
   name: "IndexPage",
   components: {
-    Taps,
-    SearchFilter,
     CardBox
   },
 };
